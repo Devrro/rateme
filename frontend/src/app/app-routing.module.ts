f6e227277064e2b06d6bc4486bc070de8fe8fe32
Serhii Terletckiy <serhii.terletskiy.ua@gmail.com>
2023-01-26 18:47:09 +0200

    bug fixes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
+import {Routes} from '@angular/router';
 import {LoginComponent} from "./compoenents/login/login.component";
 import {SignupComponent} from "./compoenents/signup/signup.component";
 import {HomeComponent} from "./compoenents/home/home.component";
