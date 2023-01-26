386e6ae0af7dea29d7cdf25fab08bc531340058b
Serhii Terletckiy <serhii.terletskiy.ua@gmail.com>
2023-01-26 19:01:03 +0200

    bug fixes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {Routes} from '@angular/router';
-import {LoginComponent} from "./compoenents/login/login.component";
-import {SignupComponent} from "./compoenents/signup/signup.component";
-import {HomeComponent} from "./compoenents/home/home.component";
-import {ProfileComponent} from "./compoenents/profile/profile.component";
-import {UserProfileComponent} from "./compoenents/profile/user-profile/user-profile.component";
-import {QrCodesPageComponent} from "./compoenents/profile/qr-codes-page/qr-codes-page.component";
-import {QrReviewsPageComponent} from "./compoenents/profile/qr-reviews-page/qr-reviews-page.component";
-import {QrCodeFormComponent} from "./compoenents/profile/qr-codes-page/qr-code-form/qr-code-form.component";
-import {ReviewQrFormComponent} from "./compoenents/review-qr-form/review-qr-form.component";
+import {LoginComponent} from "./components/login/login.component";
+import {SignupComponent} from "./components/signup/signup.component";
+import {HomeComponent} from "./components/home/home.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {UserProfileComponent} from "./components/profile/user-profile/user-profile.component";
+import {QrCodesPageComponent} from "./components/profile/qr-codes-page/qr-codes-page.component";
+import {QrReviewsPageComponent} from "./components/profile/qr-reviews-page/qr-reviews-page.component";
+import {QrCodeFormComponent} from "./components/profile/qr-codes-page/qr-code-form/qr-code-form.component";
+import {ReviewQrFormComponent} from "./components/review-qr-form/review-qr-form.component";
 import {UserGuard} from "../services/guards/user.guard";
-import {TelegramInfoComponent} from "./compoenents/profile/telegram-info/telegram-info.component";
-import {ProfileActivateComponent} from "./compoenents/signup/profile-activate/profile-activate.component";
+import {TelegramInfoComponent} from "./components/profile/telegram-info/telegram-info.component";
+import {ProfileActivateComponent} from "./components/signup/profile-activate/profile-activate.component";
 import {
   ProfileActivatedComponent
-} from "./compoenents/signup/profile-activate/profile-activated/profile-activated.component";
-import {Redirect404Component} from "./compoenents/redirect-pages/redirect404/redirect404.component";
-import {PasswordRecoveryComponent} from "./compoenents/signup/password-recovery/password-recovery.component";
+} from "./components/signup/profile-activate/profile-activated/profile-activated.component";
+import {Redirect404Component} from "./components/redirect-pages/redirect404/redirect404.component";
+import {PasswordRecoveryComponent} from "./components/signup/password-recovery/password-recovery.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
